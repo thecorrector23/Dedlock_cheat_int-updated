ad951ecf667d27260815918d69234be77e808d28
thecorrector23 <talindoquiz@gmail.com>
2024-09-27 14:31:10 +0100

    Update visuals.h

--- visuals.h
+++ visuals.h
@@ -10,7 +10,6 @@
 #include <vector>
 #include <chrono>
 
-
 struct EntityCache {
     uintptr_t entityAddress;   
     uint8_t team;            
@@ -21,56 +20,26 @@ struct EntityCache {
 };
 
 
-
-
 namespace Visuals1 {
     extern std::vector<EntityCache> entityCache;  
     extern std::chrono::time_point<std::chrono::steady_clock> lastCacheUpdate;
-    extern const int cacheUpdateInterval; 
-
+    extern const int cacheUpdateInterval;
     extern bool esp_show_head;  
-
-    
     extern float boxThickness;
     extern float lineThickness;
     extern ImVec4 boxColor;
     extern ImVec4 lineColor;
-
- 
     extern bool esp_box_enabled;
     extern bool esp_line_enabled;
-
-  
     extern bool esp_health_enabled;
     extern bool show_health;
     extern bool show_max_health;
-
-    
     extern bool esp_show_address;
-
-    
     extern bool esp_show_name;
-
-
     extern bool esp_show_circle;         
     extern float esp_distance_radius;     
-
-
-    extern uintptr_t roshanAddress;      
-    extern bool roshan_display_enabled;   
-    extern bool roshan_debug_mode;    
-    extern ImVec2 roshan_position;      
-    extern float roshan_font_size;      
-
  
     void UpdateEntityCache();
-
-
     void PlayerEsp(uint8_t& local_team);
-
-
     void RenderSettingsMenu();
-
-
-    void DrawRoshanHp();
 }
