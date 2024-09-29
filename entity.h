4f24359461633f25621488c0ff141f2d54f025a9
thecorrector23 <talindoquiz@gmail.com>
2024-09-30 00:21:40 +0100

    Update entity.h

--- entity.h
+++ entity.h
@@ -4,14 +4,12 @@
 #include "math.h"
 #include <vector>
 
-
 ULONG_PTR get_entity_list();
 int get_max_entities();
 uintptr_t get_base_entity_from_index(int index, ULONG_PTR entity_list);
 std::string get_schema_name(const uintptr_t& entity);
 std::string get_designer_name(const uintptr_t& entity);
 uintptr_t find_local_player(uint8_t& local_team);
-
 
 std::vector<Vector3> get_bone_positions(uintptr_t entity);
 Vector3 get_bone_position_by_index(uintptr_t entity, int bone_index);
